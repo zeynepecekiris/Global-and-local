2d5fa80b14418ce0348918013d42f0e2dc74a56f
zeynepecekiris <zeynep.ece933@gmail.com>
2024-07-07 10:08:45 +0300

    first commit

---
+++ ece.js
@@ -0,0 +1,17 @@
+let yas = 29
+
+
+
+if(true)
+{
+    yas=35;
+
+    console.log("icerideki:", yas);
+    if (true)
+    {
+        let yas =50;
+        console.log("iceridekinin ici:", yas);
+    }
+}
+
+console.log("disaridaki:", yas);
